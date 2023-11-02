dba59a127ae3c323e2468bb16ad955d8a9717c16
Malisha Maharjan <malishamaharjan00@gmail.com>
2023-11-02 18:33:41 +0545

    Todo model

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Todo(models.Model):
+    task = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
